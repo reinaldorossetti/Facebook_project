6f0a026a3e280ea4d8bffdce316540aff67ebd3c
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-05-24 16:15:03 -0300

    Update LoginPage.java

--- com.facebook.automation/src/test/java/pageObject/LoginPage.java
+++ com.facebook.automation/src/test/java/pageObject/LoginPage.java
@@ -24,7 +24,7 @@ public class LoginPage extends AbstractPage {
 	    "email", "email",
 	    "password", "#pass",
 	    "button", "#loginbutton",
-	    "menu", "//*[@id='fb-timeline-cover-name']"
+	    "menu", ".//*[@id=\'fb-timeline-cover-name\']"
 	    );
 	
 	public  LoginPage login(String email) throws InterruptedException {
