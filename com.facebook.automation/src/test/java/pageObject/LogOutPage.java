cbcdfec6196e685a2743ef14bd11404c9cb983a0
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-05-24 16:35:24 -0300

    Update LogOutPage.java

--- com.facebook.automation/src/test/java/pageObject/LogOutPage.java
+++ com.facebook.automation/src/test/java/pageObject/LogOutPage.java
@@ -18,6 +18,14 @@ public class LogOutPage extends AbstractPage {
 	public functions functions = new functions();
 	public String delay_w = functions.getConfig("delay_wait");
 	public int delay_wait = Integer.parseInt(delay_w);
+	
+	Map<String, String> map = ImmutableMap.of(
+	    "navigation_label_id", "userNavigationLabel",
+	    "navigation_label_css", "#userNavigationLabel",
+	    "submenu", ".navSubmenu.__MenuItem:nth-last-of-type(5)",
+	    "button", "#loginbutton",
+	    "menu", "fb-timeline-cover-name"
+);
 
 	public LogOutPage logout() throws InterruptedException {
 	
@@ -26,14 +34,16 @@ public class LogOutPage extends AbstractPage {
 
 		try{ 
 			// Open menu Log Out
-			functions.searchAndClick(By.cssSelector("#userNavigationLabel"),delay_wait);
+			functions.searchAndClick(By.cssSelector(map.get("navigation_label_id")), delay_wait);
 			// select five option of submenu, starting of the end. Because another language should be different of log out/sair. 
-			functions.searchAndClick(By.cssSelector("._54ni.navSubmenu.__MenuItem:nth-last-of-type(5)"),delay_wait);
+			functions.searchAndClick(By.cssSelector(map.get("submenu")), delay_wait);
 			
 		}
 		catch(Exception e){
-			System.out.println("error not found element");
-			functions.searchAndClick(By.cssSelector("._54ni.navSubmenu.__MenuItem:nth-last-of-type(5)"),delay_wait);
+			System.out.println("error not found element, try again!");
+			functions.searchAndClick(By.cssSelector(map.get("navigation_label_css")), delay_wait);
+			// select five option of submenu, starting of the end. Because another language should be different of log out/sair. 
+			functions.searchAndClick(By.cssSelector(map.get("submenu")), delay_wait);
 		}	
 		
 		return new LogOutPage(driver);
